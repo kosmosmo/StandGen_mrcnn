786781662a77f3721d4d70fca0958c9caf7506c7
momo <kosmosmo@gmail.com>
2019-05-04 21:48:55 -0400

    track point

--- cvshit.py
+++ cvshit.py
@@ -1,87 +1,105 @@
 import cv2,math
 import numpy as np
-bg = cv2.imread('demo/2.jpg',-1)
-fg = cv2.imread('demo/kqq.png',-1)
-st = cv2.imread('demo/kq.jpg')
-stp = cv2.imread('demo/kqq.png',-1)
 
-def createPng(h,w):
-    img_height, img_width = h,w
-    n_channels = 4
-    transparent_img = np.zeros((img_height, img_width, n_channels), dtype=np.uint8)
-    # Save the image for visualization
-    return transparent_img
-
-def mergePng(bg,img):
-    imgMask = img[:,:,3:]
-    img = img[:,:,:-1]
-    bgMask = 255 - imgMask
-    imgMask = cv2.cvtColor(imgMask, cv2.COLOR_GRAY2BGR)
-    bgMask = cv2.cvtColor(bgMask, cv2.COLOR_GRAY2BGR)
-    bgNew =  (bg * (1 / 255.0)) * (bgMask * (1 / 255.0))
-    imgNew = (img * (1 / 255.0)) * (imgMask * (1 / 255.0))
-    return np.uint8(cv2.addWeighted(bgNew, 255.0, imgNew, 255.0, 0.0))
-
-def cropImg(img,x,y,w,h):
-    return img[y:y+h, x:x+w]
+#Y,500 X,200
+class frame():
+    def __init__(self,p0,p1,fgCenterPoints,bg,fg):
+        self.p1 = p1
+        self.p0 = p0
+        self.bg = bg
+        self.fg = fg
+        self.fgCenterPoints = fgCenterPoints
+    def main(self):
+        ratio = self.getRatio()
+        ar = self.getTran(ratio)
+        img2 = self.resize(ratio)
+        arX = ar[1]
+        arY = ar[0]
+        self.combine_two_color_images_with_anchor(img2, self.bg, arX, arY)
 
 
-def cropImg2(w,h,p0,p1):
-    x1 = w*p0[0]
-    x2 = w*p1[0]
-    y1 = h*p0[1]
-    y2 = h*p1[1]
-    print (x1,y1,x2,y2)
-    dist = math.hypot(x2 - x1, y2 - y1)
-    ratio = dist/100
-    return ratio
+    def resize(self,ratio):
+        nwidth = int(self.fg.shape[1]*ratio)
+        nheight = int(self.fg.shape[0]*ratio)
+        resized = cv2.resize(self.fg, (nwidth,nheight), interpolation=cv2.INTER_AREA)
+        return resized
 
+    def createPng(self,h,w):
+        img_height, img_width = h,w
+        n_channels = 4
+        transparent_img = np.zeros((img_height, img_width, n_channels), dtype=np.uint8)
+        # Save the image for visualization
+        return transparent_img
 
-def combine_two_color_images_with_anchor(img1, img2, arY, arX):
-    tx = ty = 0
-    foreground, background = img1.copy(), img2.copy()
-    # Check if the foreground is inbound with the new coordinates and raise an error if out of bounds
-    bgH = background.shape[0]
-    bgW = background.shape[1]
-    fgH = foreground.shape[0]
-    fgW = foreground.shape[1]
-    if fgH+arY > bgH:
-        foreground = cropImg(foreground,0,0,fgW,fgH-(fgH+arY-bgH))
-    if fgW+arX > bgW:
-        foreground = cropImg(foreground,0,0,fgW-(fgW+arX-bgW),fgH)
-    if arY <0 :
-        foreground = cropImg(foreground,0,abs(arY),fgW,fgH)
-        ty = arY
-        arY = 0
-    if arX <0 :
-        foreground = cropImg(foreground,abs(arX),0,fgW,fgH)
-        tx = arX
-        arX = 0
-    cv2.imshow('composited image', foreground)
-    cv2.waitKey(0)
-    # do composite at specified location
-    start_y = arY
-    start_x = arX
-    end_y = arY+fgH+ty
-    end_x = arX+fgW+tx
-    cv2.waitKey(0)
-    cv2.waitKey(0)
-    blended_portion = mergePng(background[start_y:end_y, start_x:end_x,:],foreground)
-    cv2.imshow('composited image', background)
-    """
-    blended_portion = cv2.addWeighted(foreground,
-                alpha,
-                background[start_y:end_y, start_x:end_x,:],
-                1 - alpha,
-                0,
-                background)
-    """
-    background[start_y:end_y, start_x:end_x,:] = blended_portion
-    cv2.imshow('composited image', background)
-    cv2.waitKey(0)
+    def mergePng(self,bg,img):
+        imgMask = img[:,:,3:]
+        img = img[:,:,:-1]
+        bgMask = 255 - imgMask
+        imgMask = cv2.cvtColor(imgMask, cv2.COLOR_GRAY2BGR)
+        bgMask = cv2.cvtColor(bgMask, cv2.COLOR_GRAY2BGR)
+        bgNew =  (bg * (1 / 255.0)) * (bgMask * (1 / 255.0))
+        imgNew = (img * (1 / 255.0)) * (imgMask * (1 / 255.0))
+        return np.uint8(cv2.addWeighted(bgNew, 255.0, imgNew, 255.0, 0.0))
 
-combine_two_color_images_with_anchor(fg,bg,100,300)
+    def cropImg(slef,img,x,y,w,h):
+        return img[y:y+h, x:x+w]
 
+    def getRatio(self):
+        w = self.bg.shape[1]
+        h = self.bg.shape[0]
+        x1 = w*self.p0.x
+        x2 = w*self.p1.x
+        y1 = h*self.p0.y
+        y2 = h*self.p1.y
+        dist = math.hypot(x2 - x1, y2 - y1)
+        ratio = dist/100
+        return ratio
 
+    def getTran(self,ratio):
+        arY = self.bg.shape[0]*self.p1.y-(self.fgCenterPoints[0]*ratio)
+        arX = self.bg.shape[1]*self.p1.x-(self.fgCenterPoints[1]*ratio)
+        return (int(arX),int(arY))
 
+    def combine_two_color_images_with_anchor(self,img1, img2, arY, arX):
+        tx = ty = 0
+        foreground, background = img1.copy(), img2.copy()
+        # Check if the foreground is inbound with the new coordinates and raise an error if out of bounds
+        bgH = background.shape[0]
+        bgW = background.shape[1]
+        fgH = foreground.shape[0]
+        fgW = foreground.shape[1]
+        if fgH+arY > bgH:
+            foreground = self.cropImg(foreground,0,0,fgW,fgH-(fgH+arY-bgH))
+        if fgW+arX > bgW:
+            foreground = self.cropImg(foreground,0,0,fgW-(fgW+arX-bgW),fgH)
+        if arY <0 :
+            foreground = self.cropImg(foreground,0,abs(arY),fgW,fgH)
+            ty = arY
+            arY = 0
+        if arX <0 :
+            foreground = self.cropImg(foreground,abs(arX),0,fgW,fgH)
+            tx = arX
+            arX = 0
+        cv2.imshow('composited image', foreground)
+        cv2.waitKey(0)
+        # do composite at specified location
+        start_y = arY
+        start_x = arX
+        end_y = arY+fgH+ty
+        end_x = arX+fgW+tx
+        cv2.waitKey(0)
+        cv2.waitKey(0)
+        blended_portion = self.mergePng(background[start_y:end_y, start_x:end_x,:],foreground)
+        cv2.imshow('composited image', background)
+        """
+        blended_portion = cv2.addWeighted(foreground,
+                    alpha,
+                    background[start_y:end_y, start_x:end_x,:],
+                    1 - alpha,
+                    0,
+                    background)
+        """
+        background[start_y:end_y, start_x:end_x,:] = blended_portion
+        cv2.imshow('composited image', background)
+        cv2.waitKey(0)
 
--- poseEst.py
+++ poseEst.py
@@ -13,17 +13,26 @@ formatter = logging.Formatter('[%(asctime)s] [%(name)s] [%(levelname)s] %(messag
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
-original_image = 'demo/9.jpg'
-image = cv2.imread(original_image)
+original_image = 'demo/2.jpg'
+image = cv2.imread(original_image,-1)
 w, h = model_wh('432x368')
 e = TfPoseEstimator(get_graph_path('mobilenet_thin'), target_size=(w, h))
 humans = e.inference(image, resize_to_default=(w > 0 and h > 0), upsample_size=4)
-image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False)
+print (humans[0].body_parts[1])
+print (humans[0].body_parts[1].x,humans[0].body_parts[1].y)
+print (humans[0].body_parts[0])
+image = TfPoseEstimator.draw_humans_new(image, humans, imgcopy=False)
 cv2.imshow('tf-pose-estimation result', image)
 k = cv2.waitKey(0)
 if k == 27:
-
     cv2.destroyAllWindows()
+
+bg =  image
+fg = cv2.imread('demo/kqq.png',-1)
+
+import cvshit
+a = cvshit.frame(humans[0].body_parts[0],humans[0].body_parts[1],(500,200),bg,fg)
+a.main()
 
 
 
